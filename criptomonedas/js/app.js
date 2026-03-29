98dfd2d3be0909388ecd46a940315022ff4cbd3d
Koridormi <258958198+Koridormi@users.noreply.github.com>
2026-03-29 17:22:05 -0300

    Async-Await Added

--- criptomonedas/js/app.js
+++ criptomonedas/js/app.js
@@ -23,16 +23,16 @@ document.addEventListener('DOMContentLoaded', () => {
     monedaSelect.addEventListener('change', leerValor);
 });
 
-function consultarCriptomonedas() {
-    const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`;
-    fetch(url)
-        .then( (respuesta) => {
-            return respuesta.json();
-        })
-        .then( (resultado) => {
-            return obtenerCriptomonedas(resultado.Data);
-        })
-        .then( (criptomonedas) => selectCriptomonedas(criptomonedas));
+async function consultarCriptomonedas() {
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        const criptomonedas = await obtenerCriptomonedas(resultado.Data);
+        selectCriptomonedas(criptomonedas);
+    } catch (error) {
+        console.log(error);
+    };
 };
 
 function selectCriptomonedas(criptomonedas) {
@@ -79,17 +79,18 @@ function mostrarAlerta(msg) {
     };
 };
 
-function consultarAPI() {
+async function consultarAPI() {
     const { moneda, criptomoneda } = objBusqueda;
+
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
     mostrarSpinner();
-    fetch(url)
-        .then( (respuesta) => {
-            return respuesta.json();
-        })
-        .then( (cotizacion) => {
-            mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
-        });
+    try {
+        const respuesta = await fetch(url);
+        const cotizacion = await respuesta.json();
+        mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+    } catch (error) {
+        console.log(error);
+    };
 };
 
 function mostrarCotizacionHTML(cotizacion) {
